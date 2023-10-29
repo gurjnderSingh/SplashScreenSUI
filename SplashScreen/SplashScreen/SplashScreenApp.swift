3d20b4679ba4700b7d4188085ef1e866a769c6b2
gurjnderSingh <gurjindersingh663@gmail.com>
2023-10-29 20:07:29 +0530

    inital commit

---
+++ SplashScreen/SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by Gurjinder Singh on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
