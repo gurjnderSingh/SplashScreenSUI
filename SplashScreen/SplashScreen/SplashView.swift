ca92b7057074ebd4a5a13a098fdd8a200b9589d1
gurjnderSingh <gurjindersingh663@gmail.com>
2023-10-29 20:40:20 +0530

    done

---
+++ SplashScreen/SplashScreen/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  SplashScreen
+//
+//  Created by Gurjinder Singh on 29/10/23.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
